8d0ca1ca52917de6fe5377c85dcf884f839408c9
fogus <mefogus@gmail.com>
2012-03-27 10:03:55 -0400

    Filling in index detail.

--- src/cljs/himera/client/repl.cljs
+++ src/cljs/himera/client/repl.cljs
@@ -65,7 +65,7 @@
           (fn []
             (set! js/controller
                   (doto (js/jQuery "#console")
-                    (.console (map->js {:welcomeMessage "Himera REPL v0.1.0"
+                    (.console (map->js {:welcomeMessage "Himera REPL v0.1.5"
                                         :promptLabel "cljs.user> "
                                         :commandValidate on-validate
                                         :commandHandle on-handle
