0d4b9361d71aa56d2386f809f9b4e8f541579243
fogus <mefogus@gmail.com>
2013-07-25 11:46:24 -0400

    Updated copyright

--- src/clj/himera/server/cljs.clj
+++ src/clj/himera/server/cljs.clj
@@ -1,4 +1,4 @@
-; Copyright (c) 2012 Fogus and Relevance Inc. All rights reserved.  The
+; Copyright (c) 2012, 2013 Fogus and Relevance Inc. All rights reserved.  The
 ; use and distribution terms for this software are covered by the Eclipse
 ; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
 ; which can be found in the file COPYING the root of this
